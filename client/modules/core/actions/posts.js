453beb050d3a32a93cfae24a61cd2288d4fdc3db
Josh <josh@aluminati.net>
2016-07-20 09:57:14 +0100

    change creating post action to validate against simpleSchema and provide errors back to the autoforum

--- client/modules/core/actions/posts.js
+++ client/modules/core/actions/posts.js
@@ -1,20 +1,31 @@
+import {PostsSchema} from '/lib/collections/posts';
+import {validateOnly, invalidKeys, forumFieldsExist} from './../../../lib/validate_schema';
+
 export default {
-  create({Meteor, LocalState, FlowRouter}, title, content) {
-    if (!title || !content) {
-      return LocalState.set('SAVING_ERROR', 'Title & Content are required!');
-    }
+  create({Meteor, LocalState, FlowRouter}, forumFields) {
+    const _id = Meteor.uuid();
+    const context = validateOnly(PostsSchema, forumFields, ['title', 'content']);
 
     LocalState.set('SAVING_ERROR', null);
 
-    const id = Meteor.uuid();
-    // There is a method stub for this in the config/method_stubs
-    // That's how we are doing latency compensation
-    Meteor.call('posts.create', id, title, content, (err) => {
-      if (err) {
-        return LocalState.set('SAVING_ERROR', err.message);
-      }
-    });
-    FlowRouter.go(`/post/${id}`);
+    if(forumFieldsExist(forumFields) && context.isValid())
+    {
+      // There is a method stub for this in the config/method_stubs
+      // That's how we are doing latency compensation
+      Meteor.call('posts.create', _id, forumFields, (err) =>
+      {
+        if(err)
+        {
+          return LocalState.set('SAVING_ERROR', err.message);
+        }
+      });
+
+      FlowRouter.go(`/post/${_id}`);
+
+      return;
+    }
+
+    return LocalState.set('SAVING_ERROR', invalidKeys(context));
   },
 
   clearErrors({LocalState}) {
