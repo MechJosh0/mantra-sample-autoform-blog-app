8c77d1ecbe92e1bcf25a23d8a519af5fe03ff47f
Josh <josh@aluminati.net>
2016-07-20 09:42:53 +0100

    component to include prop types

--- client/modules/core/components/newpost.js
+++ client/modules/core/components/newpost.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactAutoForm from 'meteor-react-autoform';
 import PostsSchema from '/lib/schema/posts';
 
-const NewPost = () => (
+const NewPost = ({create, errors}) => (
   <div>
     <h2>Add New Post</h2>
     <ReactAutoForm
+      errors={errors}
       muiTheme={true}
-      onSubmit={this.props.create}
+      onSubmit={create}
       schema={PostsSchema}
       type="insert"
       useFields={['title', 'content']}
     />
   </div>
 );
+
+NewPost.propTypes = {
+  errors: React.PropTypes.array,
+  create: React.PropTypes.func.isRequired
+};
 
 export default NewPost;
