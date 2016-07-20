99f7a1d0b23881074162a9dde865e1183085e6ad
Josh <josh@aluminati.net>
2016-07-20 12:05:49 +0100

    turn the manual forum into an autoform using our schema

--- client/modules/comments/components/create_comment.js
+++ client/modules/comments/components/create_comment.js
@@ -1,34 +1,29 @@
 import React from 'react';
+import ReactAutoForm from 'meteor-react-autoform';
+import CommentsSchema from '/lib/schema/comments';
 
-class CreateComment extends React.Component {
-  render() {
-    const {error} = this.props;
-    return (
-      <div>
-        {error ? this._renderError(error) : null}
-        <textarea ref='text' placeholder='Enter your comment here.'>
+const CreateComment = ({create, errors, postId, submitted}) => (
+  <div>
+    <h2>Add New Post</h2>
+    <ReactAutoForm
+      buttonLabel="Add Comment"
+      buttonProps={{disabled: true}}
+      errors={errors}
+      muiTheme={true}
+      onSubmit={create}
+      onSubmitExtra={{postId}}
+      schema={CommentsSchema}
+      type="insert"
+      useFields={['text']}
+    />
+  </div>
+);
 
-        </textarea>
-        <br />
-        <button onClick={this._create.bind(this)}>Add Comment</button>
-      </div>
-    );
-  }
-
-  _create() {
-    const text = this.refs.text.value;
-    const {create, postId} = this.props;
-    create(postId, text);
-    this.refs.text.value = '';
-  }
-
-  _renderError(error) {
-    return (
-      <div className='error'>
-        {error}
-      </div>
-    );
-  }
-}
+CreateComment.propTypes = {
+  errors: React.PropTypes.array,
+  create: React.PropTypes.func.isRequired,
+  postId: React.PropTypes.string.isRequired,
+  submitted: React.PropTypes.bool
+};
 
 export default CreateComment;
